bf256e8de5b2d4e1944e1978b12c47f05f2d021f
John Gibson <johgibso@gmail.com>
2017-08-25 17:57:29 -0400

    Put mac libs in a mac dir.

--- deploy-build-mac.sh
+++ deploy-build-mac.sh
@@ -5,15 +5,15 @@
 appname=RTcmixShell
 # for some reason, Qt Creator keeps wanting to write the location
 # of rtcmix_embedded.dylib as the one in my RTcmix tree, instead
-# of the one in RTcmixShell/lib.
+# of the one in RTcmixShell/lib/mac.
 exepath=/Users/johgibso/docs/development/Qt-mine/build-${appname}-Desktop_Qt_5_9_1_clang_64bit-Release/${appname}.app/Contents/MacOS/${appname}
-install_name_tool -change /Users/johgibso/rtcmix/lib/librtcmix_embedded.dylib /Users/johgibso/docs/development/Qt-mine/${appname}/lib/librtcmix_embedded.dylib $exepath
+install_name_tool -change /Users/johgibso/rtcmix/lib/mac/librtcmix_embedded.dylib /Users/johgibso/docs/development/Qt-mine/${appname}/lib/mac/librtcmix_embedded.dylib $exepath
 cd ../build-${appname}-Desktop_Qt_5_9_1_clang_64bit-Release
 macdeployqt ${appname}.app
 
 # now dylibs are copied into the app bundle, but the reference to
 # the rtcmix lib in the app is still wrong.
-install_name_tool -change /Users/johgibso/docs/development/Qt-mine/${appname}/lib/librtcmix_embedded.dylib @executable_path/../Frameworks/librtcmix_embedded.dylib $exepath
+install_name_tool -change /Users/johgibso/docs/development/Qt-mine/${appname}/lib/mac/librtcmix_embedded.dylib @executable_path/../Frameworks/librtcmix_embedded.dylib $exepath
 
 # replace the Info.plist with our own
 cp -f ../${appname}/Info.plist ${appname}.app/Contents/
