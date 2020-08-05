38ae3b8f19935e616486671d70240ecefd8df61c
John Gibson <johgibso@gmail.com>
2020-08-05 16:26:49 -0400

    Assists in signing the app.

---
+++ sign.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+identity='Developer ID Application: Indiana University (5J69S77A7G)'
+appname=RTcmixShell
+qtversion=5_15_0
+projectdir=/Users/johgibso/docs/development/Qt-mine/${appname}
+entitlements=${projectdir}/Entitlements.plist
+
+builddir=${projectdir}/../build-${appname}-Desktop_Qt_${qtversion}_clang_64bit-Release
+app=${builddir}/${appname}.app
+tool=codesign
+#tool=echo
+
+${tool} --sign "$identity" --deep --force --verbose --options runtime --entitlements ${entitlements} ${app}
+${tool} --verify --verbose --deep --strict ${app}
+
