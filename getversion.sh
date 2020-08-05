70b550fd4fc73e0fce878ac054bbcc5497f28fb1
John Gibson <johgibso@gmail.com>
2020-08-05 18:55:43 -0400

    Example of extracting version number from the source.

---
+++ getversion.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# In main.cpp, the version number is declared on this line...
+# #define APP_VERSION_STR             "1.0.9"
+# We extract the line with grep, remove double quotes, strip newline, and
+# store the 3rd column into $version.
+version=`grep "define APP_VERSION_STR" main.cpp | sed 's/"//g' \
+	| tr -d '\n' | awk '/define/ {print $3}'`
+echo ${version}
