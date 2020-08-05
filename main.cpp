8234853826452bb5ff8f2032c91bb9f6dc1f4a68
John Gibson <johgibso@gmail.com>
2020-08-05 18:45:51 -0400

    Bump version number.

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@
 #define APP_ORGANIZATION_NAME       "RTcmix"
 #define APP_ORGANIZATION_DOMAIN     "rtcmix.org"
 #define APP_NAME                    "RTcmixShell"
-#define APP_VERSION_STR             "v1.0.8"
+#define APP_VERSION_STR             "1.0.9"
 
 int main(int argc, char *argv[])
 {
