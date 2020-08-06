96921ca9c5a7cc694bdf8df2a97818f58236d36e
John Gibson <johgibso@gmail.com>
2020-08-05 21:04:35 -0400

    Fix line ending removal. Add user warning about matching version number in Info.plist.

--- getversion.sh
+++ getversion.sh
@@ -5,5 +5,6 @@
 # We extract the line with grep, remove double quotes, strip newline, and
 # store the 3rd column into $version.
 version=`grep "define APP_VERSION_STR" main.cpp | sed 's/"//g' \
-	| tr -d '\n' | awk '/define/ {print $3}'`
+	| tr -d '\r\n' | awk '/define/ {print $3}'`
 echo ${version}
+echo "Check that version in Info.plist matches!"
